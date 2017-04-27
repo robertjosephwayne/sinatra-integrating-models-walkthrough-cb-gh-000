372023c9e0ba7a3a751a10242eab8c71e45d6704
robertjosephwayne <robertjwayne@gmail.com>
2017-04-27 19:44:05 -0400

    Create results page to display text analysis

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require_relative 'config/environment'
-require_relative 'models/textanalyzer.rb'
+require_relative 'models/textanalyzer'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/' do
-  text_from_user = params[:user_text]
-  erb :results
-end
-
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    erb :results
+  end
 
 end
